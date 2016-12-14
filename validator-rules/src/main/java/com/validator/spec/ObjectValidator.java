8c3fb38f9a9c7731251a825a60a69875068c3204
Avishek Seal <seal.avishek@yahoo.com>
2016-12-14 15:59:20 +0530

    comment added

--- validator-rules/src/main/java/com/validator/spec/ObjectValidator.java
+++ validator-rules/src/main/java/com/validator/spec/ObjectValidator.java
@@ -3,5 +3,15 @@ package com.validator.spec;
 @FunctionalInterface
 public interface ObjectValidator {
 
+	/**
+	 * this method is used to validate object of any class having
+	 * annotation of {com.validator.type} package over the properties
+	 * of that class.
+	 * 
+	 * @author Avishek Seal
+	 * @since 14-Dec-2016
+	 * @param t
+	 * @throws Exception
+	 */
 	<T> void validate(T t) throws Exception;
 }
