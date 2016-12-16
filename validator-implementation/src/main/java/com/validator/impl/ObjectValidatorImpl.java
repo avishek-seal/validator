22751fdf2b691a17a38ea9f526dc71e4d627dae8
Avishek Seal <seal.avishek@yahoo.com>
2016-12-16 18:14:15 +0530

    validator implementation code comment and cleanup

--- validator-implementation/src/main/java/com/validator/impl/ObjectValidatorImpl.java
+++ validator-implementation/src/main/java/com/validator/impl/ObjectValidatorImpl.java
@@ -2,7 +2,12 @@ package com.validator.impl;
 
 import com.validator.spec.AbstractRegisterValidator;
 import com.validator.spec.ObjectValidator;
-
+/**
+ * this class is used to modify the Validation Specification
+ * 
+ * @author Avishek Seal
+ * @since Dec 16, 2016
+ */
 public class ObjectValidatorImpl extends AbstractRegisterValidator implements ObjectValidator{
 
 	@Override
